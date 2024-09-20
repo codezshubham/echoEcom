af177fd4cb93a43b5921dedcfccf24c949e8b82f
codezshubham <kumarshubhamgupta478@gmail.com>
2024-09-20 20:42:23 +0530

    Signin Added

--- src/customer/Components/Navbar/Navigation.jsx
+++ src/customer/Components/Navbar/Navigation.jsx
@@ -223,12 +223,12 @@ export default function Navigation() {
 
                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
                   <div className="flow-root">
-                    <a
-                      href="/"
-                      className="-m-2 block p-2 font-medium text-gray-900"
+                  <Button
+                      onClick={handleOpen}
+                      className="text-sm font-medium text-gray-700 hover:text-gray-800"
                     >
-                      Sign in
-                    </a>
+                      Signin
+                    </Button>
                   </div>
                 </div>
 
